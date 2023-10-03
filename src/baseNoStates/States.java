b6046ffead41dbcf5a5813f29113e1033725f5c2
marcmejias <todoimproviso@gmail.com>
2023-10-03 17:42:41 +0200

    Added States class.

---
+++ src/baseNoStates/States.java
@@ -0,0 +1,7 @@
+package baseNoStates;
+
+public final class States {
+    // possible states in door requests
+    public static final String LOCKED = "locked";
+    public static final String UNLOCKED = "unlocked";
+}
