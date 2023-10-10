63deaf1f90679c858e24001ed387c9c194d73d1a
es2021uab-1531174 <1531174@uab.cat>
2023-10-10 16:36:47 +0200

    class Partitions v2

--- src/baseNoStates/Partition.java
+++ src/baseNoStates/Partition.java
@@ -1,46 +1,27 @@
 package baseNoStates;
 
-public class Partition {
-  private final String building;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-  private final String basement;
+public final class Partition {
+  private static ArrayList<Partition> partitions;
 
-  private final String parking;
+  public Partition(String basement) {
 
-  private final String ground_floor;
-
-  private final String hall;
-
-  private final String room1;
-
-  private final String room2;
-
-  private final String room3;
-
-  private final String corridor;
-
-  private final String IT;
-
-  private final String floor1;
-
-  private final String stairs;
-
-  private final String exterior;
-
-
-  public Partition(String building, String basement, String parking, String ground_floor, String hall, String room1, String room2, String room3, String corridor, String it, String floor1, String stairs, String exterior) {
-    this.building = building;
-    this.basement = basement;
-    this.parking = parking;
-    this.ground_floor = ground_floor;
-    this.hall = hall;
-    this.room1 = room1;
-    this.room2 = room2;
-    this.room3 = room3;
-    this.corridor = corridor;
-    IT = it;
-    this.floor1 = floor1;
-    this.stairs = stairs;
-    this.exterior = exterior;
   }
-}
+
+  public static void makePartition() {
+      // basement
+      Partition p1 = new Partition("basement");
+      Partition p2 = new Partition("ground floor");
+      Partition p3 = new Partition("floor 1");
+
+
+      partitions = new ArrayList<>(Arrays.asList(p1,p2,p3));
+    }
+      public static ArrayList<Partition> getPartitions() {
+      System.out.println(partitions);
+      return partitions;
+    }
+
+  }
