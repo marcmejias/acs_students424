6d8acc894f84fe07d8142f8b0c1c10b7fc423f75
es2021uab-1531174 <1531174@uab.cat>
2023-11-23 14:55:42 +0100

    area actulizada

--- src/baseNoStates/Area.java
+++ src/baseNoStates/Area.java
@@ -1,18 +1,22 @@
 package baseNoStates;
 
-public abstract class Area { // Areas are the non-specified version of Partitions and Spaces, i.e. their shared values
+public abstract class Area {
+    // Areas are the non-specified version of Partitions and Spaces,
+    // i.e. their shared values
     protected final String id;
     protected static Partition rootArea;
 
-    public Area (String id, Partition root){
-        this.id = id;
-        rootArea = root;
+    public Area(final String idLocal, final Partition rootLocal) {
+        this.id = idLocal;
+        rootArea = rootLocal;
         if (this.rootArea != null) { //If this area has a father...
-            this.rootArea.addChild(this); //we notify the father of this area that it has a new children
+            this.rootArea.addChild(this);
+            //we notify the father of this area that it has a new children
         }
     }
     public String getId() {
         return id;
     }
-    public abstract void acceptVisitor(Visitor visitor); // Check Partitions or Space for specific implementation
+    public abstract void acceptVisitor(Visitor visitor);
+    // Check Partitions or Space for specific implementation
 }
